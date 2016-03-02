baa13e7eabd22b1c0deaa257222ad15e344ad793
Christoph Guttandin <chrisguttandin@media-codings.com>
2016-03-02 18:58:11 +0100

    update DynamoDB Local

--- src/module.js
+++ src/module.js
@@ -4,9 +4,9 @@ var spawn = require('child_process').spawn;
 
 module.exports.spawn = function () {
     return spawn('java', [
-        '-Djava.library.path=../lib/dynamodb_local_2016-01-07_1.0/DynamoDBLocal_lib',
+        '-Djava.library.path=../lib/dynamodb_local_2016-03-01/DynamoDBLocal_lib',
         '-jar',
-        '../lib/dynamodb_local_2016-01-07_1.0/DynamoDBLocal.jar',
+        '../lib/dynamodb_local_2016-03-01/DynamoDBLocal.jar',
         '-inMemory'
     ], {
         cwd: __dirname
