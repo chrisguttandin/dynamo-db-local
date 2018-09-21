20aa98ad4b087d88a0ec598b658f9844f36518a0
Christoph Guttandin <chrisguttandin@media-codings.com>
2018-09-21 21:51:54 +0200

    feat: allow setting the path of the database file

--- src/module.js
+++ src/module.js
@@ -1,12 +1,17 @@
 const { spawn } = require('child_process');
 
-module.exports.spawn = function ({ port = null } = { }) {
+module.exports.spawn = function ({ path = null, port = null } = { }) {
     const args = [
         '-Djava.library.path=../lib/dynamodb_local_2018-04-13/DynamoDBLocal_lib',
         '-jar',
-        '../lib/dynamodb_local_2018-04-13/DynamoDBLocal.jar',
-        '-inMemory'
+        '../lib/dynamodb_local_2018-04-13/DynamoDBLocal.jar'
     ];
+
+    if (path === null) {
+        args.push('-inMemory');
+    } else {
+        args.push('-dbPath', path);
+    }
 
     if (port !== null) {
         args.push('-port', port.toString());
