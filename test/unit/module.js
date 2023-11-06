b1ab191dde674d73912928a5e2ad2e4e6c7a2084
Christoph Guttandin <chrisguttandin@media-codings.com>
2023-11-06 23:22:26 +0100

    feat: update DynamoDB Local to version 2.1.0

--- test/unit/module.js
+++ test/unit/module.js
@@ -8,7 +8,7 @@ describe('dynamoDbLocal', function () {
     let version;
 
     before(async () => {
-        date = '2023-06-28';
+        date = '2023-10-23';
 
         const readme = await readFile(join(__dirname, `../../lib/dynamodb_local_${date}/README.txt`), { encoding: 'utf-8' });
 
