d1438ccd5866a6107f73c043d673b5388dad0500
Christoph Guttandin <chrisguttandin@media-codings.com>
2026-08-21 00:07:53 +0200

    feat: update DynamoDB Local to version 3.3.1

--- test/unit/factories/spawn.js
+++ test/unit/factories/spawn.js
@@ -11,7 +11,7 @@ describe('spawn()', function () {
     let version;
 
     before(async () => {
-        date = '2026-01-16';
+        date = '2026-05-28';
 
         const readme = await readFile(join(__dirname, `../../../lib/dynamodb_local_${date}/README.txt`), { encoding: 'utf-8' });
 
